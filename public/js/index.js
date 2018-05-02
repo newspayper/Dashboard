e6106afb5da43797e9124e92823acf22e3ff4d9a
ElBulgaro <34547278+ElBulgaro@users.noreply.github.com>
2018-05-02 17:00:43 +0200

    Ajouts
    HTML
    Modification Exemple Statique Modal
    Ajouts Champs Formulaire
    
    JS
    Affichage du Modal (sur Exemple Statique, pas sur Elements ajoutés)
    Ajouts placeholder pour futures fonctions

--- public/js/index.js
+++ public/js/index.js
@@ -1,24 +1,26 @@
-  // Initialize Firebase
-  var config = {
+// Initialize Firebase
+var config = {
     apiKey: "AIzaSyAqCBAqOr76xKL-eMXZQxGfWVfLO7WIgw0",
     authDomain: "titres-presse-npp.firebaseapp.com",
     databaseURL: "https://titres-presse-npp.firebaseio.com",
     projectId: "titres-presse-npp",
     storageBucket: "titres-presse-npp.appspot.com",
     messagingSenderId: "73707316345"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
+
+var today = new Date();
 
 // Call Database
 
-  var publicationsRef = firebase.database().ref().child("publications");
+var publicationsRef = firebase.database().ref().child("publications");
 
 // Get Snapshot
 
-  publicationsRef.on("child_added", snap => {
+publicationsRef.on("child_added", snap => {
 
-// Collect Card Data
+    // Collect Card Data
 
     var URL_couv = snap.child("URL_couv").val();
     // On enregistre la date de parution sous forme de date
@@ -29,23 +31,20 @@
     var titre = snap.child("titre").val();
     /* Il faut ajouter la periodicité dans la base publication pour pouvoir la traiter
     var periodicite = snap.child("periodicite").val();
-
-*/
+    */
 
     // Calcul Besoin de mise à jour publication
 
-    var periodicite = 30; // A supprimer lorsque periodicite sera issu de la base
-
-    var today = new Date(); // A placer avant d'entrer dans la boucle pour ne pas reset la variable a chaque child
+    var periodicite = 30; // A supprimer lorsque periodicite sera issue de la base
 
     // Calcul de la difference entre les dates en jours (arrondi au supérieur)
     var diff = Math.ceil((today - date_parution) / 86400000);
 
-    console.log(diff);
+    // console.log(diff);
     
-    console.log(ribbonAlert(diff, periodicite));
+    // console.log(ribbonAlert(diff, periodicite));
 
-    // Append Array de titres à mettre à jour au fur et à mesure ?
+        // (Suggestion : Append Array de titres à mettre à jour au fur et à mesure ?)
 
 // Create Card String
 
@@ -56,10 +55,10 @@
     cardString +=    ribbonAlert(diff, periodicite)
     cardString +=   '</div>'
     cardString +=  '<div class="content">'
-    cardString +=  '<a class="header" id="nom">' + titre + ' n° ' + numero + '</a>'
+    cardString +=  '<a class="header">' + titre + ' n° ' + numero + '</a>'
     cardString +=    '<div class="meta">'
     // On transforme la date au format français avant de l'afficher
-    cardString +=      '<span class="date" id="date_parution">' + date_parution.toLocaleDateString('fr-FR') + '</span>'
+    cardString +=      '<span class="date"' + date_parution.toLocaleDateString('fr-FR') + '</span>'
     cardString +=    '</div>'
     cardString +=  '</div>'
     cardString +=  '<div class="extra content">'
@@ -82,31 +81,53 @@
 
   });
 
-
+// la fonction suivante renvoie l'élement ribbon HTML/DOM à afficher en fonction de l'urgence de MAJ (utilisé par cardString)
 function ribbonAlert (diff, periodicite) {
     var reste = periodicite - diff;
     if (reste < 3) {
-      return '<a class="ui red ribbon label" onclick="clique()">MAJ REQUISE</a>';
+      return '<a class="ui red ribbon label">MAJ REQUISE</a>';
     } else {
-      return '<a class="ui green ribbon label" onclick="clique()">RESTE '+ reste + ' JOURS</a>' ;
+      return '<a class="ui green ribbon label">RESTE '+ reste + ' JOURS</a>' ;
     }
 };
 
-function clique() { // fonction à renommer ? (displayForm ?)
-    
-    // On récupère l'ID de la carte (parent*2 de l'element cliqué [ribbon])
-    ribbonParent = $(event.target).parent().parent();
-    console.log(ribbonParent.attr('id'));
-    
-    // On modifie l'élement modal
+$(document).ready(function() {
+    //alert("ready");
+});
 
-        // code pour ajouter les élements 
 
-    // On affiche l'élément modal (fonction Semantic UI)
-    $('.ui.modal').modal('show');
+// la fonction suivante fonctionne dans tous les navigateurs mais ne marche pas sur les ribbons créés (il faut attendre la fin de la fonction firebase qui lit la base)
+$(document).ready(function() {
+    $(".ribbon").click(function() {
+        //alert("cliquage");
+        ribbonParent = $(this).parent().parent();
+        pubId = ribbonParent.attr('id');
+        console.log(pubId);
 
-    //displayForm();
+        var pubArray = createPubArray(pubId);
+        modalForm (pubArray);
+
+        // On affiche l'élément modal (fonction Semantic UI)
+        $('.ui.modal').modal('show');
+    });
+});
+
+function createPubArray(pubId){
+
+    // fonction qui crée l'array d'elements qui seront utilisés dans l'affichage du modal (pop-up)
+    var pubArray;
+
+    return pubArray;
+};
+
+function modalForm(pubArray) {
+
+    // fonction qui modifie / recrée le modal (voir HTML)
 
 };
 
+function submitPub() {
 
+    // fonction qui enregistre en base le formulaire du modal
+
+};
